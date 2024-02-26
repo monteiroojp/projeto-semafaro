3a0ecfb3f1017ca2f601e70227e82834eb1cedfe
João Pedro Monteiro <154384458+monteiroojp@users.noreply.github.com>
2024-02-26 09:40:26 -0300

    projeto-semafaro finished

--- script.js
+++ script.js
@@ -1,19 +1,34 @@
 let image = document.querySelector("img#sem")
 let buttons = document.querySelector('div#buttons')
+let colorIndex = 0
+IntervalId =  null
+
+const nextIndex = () => {
+    colorIndex < 2? colorIndex++ : colorIndex = 0
+}
 
 const trafficLight = (Event) =>{
 
 const changecolor = () => {
-    
+    let automatic = ['red','yellow','green']
+    const color = automatic[ colorIndex ]
+    colors[color]()
+    nextIndex()
 }
+
+const stopInterval = () =>{
+    clearInterval(IntervalId)
+}
+
     
 let colors = {
     red: () => image.src = 'imagens/vermelho.png',
     green: () => image.src = 'imagens/verde.png',
     yellow: () => image.src = 'imagens/amarelo.png',
-    automatic: () => changecolor()
+    automatic: () => IntervalId = setInterval(changecolor, 1000)
 }  
     var choosedButton = colors[Event.target.id]
+    stopInterval()
     colors[Event.target.id]() 
       
     
